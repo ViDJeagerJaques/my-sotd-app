ad4610e5c75c12dc31938bdd285fd8107628f2af
ViD_Atha <ooddaangg@gmail.com>
2026-05-31 01:38:54 +0700

    fix: embed supabase keys directly into next config for build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://navdlgrkdfemdariuhls.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hdmRsZ3JrZGZlbWRhcml1aGxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODQ5OTgsImV4cCI6MjA5NTU2MDk5OH0.FAn0YbjsNoQy-6pIvaCssvKzSjXMkS6iWwfNtO2KkI4",
+  },
 };
 
 export default nextConfig;
